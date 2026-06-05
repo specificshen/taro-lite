cc4a7365fc0612a91c2f7e371c82a2c2974054a5
Samuel Ouyang <oys01063538@cainiao.com>
2026-06-05 22:03:56 +0800

    chore: 收紧拦截器 abort 类型

--- packages/taro/src/api/interceptor/interceptors.ts
+++ packages/taro/src/api/interceptor/interceptors.ts
@@ -1,10 +1,22 @@
-import { isFunction, isUndefined } from '@spcsn/taro-shared';
+import { isFunction } from '@spcsn/taro-shared';
 
 import type Chain from './chain';
 
+type AbortablePromise<T> = Promise<T> & {
+  abort?: () => void;
+};
+
+function attachAbort<T>(source: AbortablePromise<T> | undefined, target: Promise<T>): AbortablePromise<T> {
+  const abortableTarget = target as AbortablePromise<T>;
+  if (isFunction(source?.abort)) {
+    abortableTarget.abort = source.abort;
+  }
+  return abortableTarget;
+}
+
 export function timeoutInterceptor(chain: Chain) {
   const requestParams = chain.requestParams;
-  let p: Promise<void>;
+  let proceedPromise: AbortablePromise<void> | undefined;
   const res = new Promise<void>((resolve, reject) => {
     const timeout: ReturnType<typeof setTimeout> = setTimeout(
       () => {
@@ -14,8 +26,8 @@ export function timeoutInterceptor(chain: Chain) {
       (requestParams && requestParams.timeout) || 60000,
     );
 
-    p = chain.proceed(requestParams);
-    p.then((res) => {
+    proceedPromise = chain.proceed(requestParams);
+    proceedPromise.then((res) => {
       if (!timeout) return;
       clearTimeout(timeout);
       resolve(res);
@@ -24,10 +36,7 @@ export function timeoutInterceptor(chain: Chain) {
       reject(err);
     });
   });
-  // @ts-ignore
-  if (!isUndefined(p) && isFunction(p.abort)) res.abort = p.abort;
-
-  return res;
+  return attachAbort(proceedPromise, res);
 }
 
 export function logInterceptor(chain: Chain) {
@@ -43,8 +52,5 @@ export function logInterceptor(chain: Chain) {
     console.log(`http <-- ${url} result:`, res);
     return res;
   });
-  // @ts-ignore
-  if (isFunction(p.abort)) res.abort = p.abort;
-
-  return res;
+  return attachAbort(p, res);
 }
