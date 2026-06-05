c582eb4fd12644e71a009d57886ee889342902ce
Samuel Ouyang <oys01063538@cainiao.com>
2026-06-05 21:56:55 +0800

    chore: 删除旧平台无用配置

--- packages/taro-vite-runner/src/utils/constants.ts
+++ packages/taro-vite-runner/src/utils/constants.ts
@@ -47,5 +47,3 @@ export const DEFAULT_TERSER_OPTIONS = {
 };
 
 export const MINI_EXCLUDE_POSTCSS_PLUGIN_NAME = ['cssModules'];
-export const H5_EXCLUDE_POSTCSS_PLUGIN_NAME = ['cssModules', 'url'];
-export const HARMONY_SCOPES = [/^@system\./, /^@ohos\./, /^@hmscore\//, /^@(jd|taro)-oh\//];
