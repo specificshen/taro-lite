75ab9b5d498834ed60fbd9378324e14b5b338b03
Samuel Ouyang <oys01063538@cainiao.com>
2026-06-06 05:52:06 +0800

    fix: 移除 service 未声明 fs-extra 依赖

--- packages/taro-service/src/utils/package.ts
+++ packages/taro-service/src/utils/package.ts
@@ -1,13 +1,14 @@
+import * as fs from 'node:fs';
 import * as path from 'node:path';
-
-import * as fs from 'fs-extra';
 
 export function getRootPath(): string {
   return path.resolve(__dirname, '../../');
 }
 
 export function getPkgVersion(): string {
-  return fs.readJSONSync(path.join(getRootPath(), 'package.json')).version;
+  const packageJsonPath = path.join(getRootPath(), 'package.json');
+  const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+  return JSON.parse(packageJsonContent).version;
 }
 
 export function printPkgVersion() {
