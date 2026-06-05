69e603c68c5942dd17696c1b3606d61e70a5b2b6
Samuel Ouyang <oys01063538@cainiao.com>
2026-06-06 06:38:17 +0800

    chore: 移除终端链接转义依赖

--- packages/taro-helper/src/terminal.ts
+++ packages/taro-helper/src/terminal.ts
@@ -1,4 +1,3 @@
-import ansiEscapes from 'ansi-escapes';
 import chalk from 'chalk';
 import supportsHyperlinks from 'supports-hyperlinks';
 
@@ -8,6 +7,10 @@ interface ITerminalLinkOptions {
   [key: string]: unknown;
 }
 
+function createTerminalLink(text: string, url: string) {
+  return `\u001B]8;;${url}\u0007${text}\u001B]8;;\u0007`;
+}
+
 function terminalLink(text: string, url: string, { target = 'stdout', fallback }: ITerminalLinkOptions = {}) {
   if (!supportsHyperlinks[target]) {
     if (fallback === false) return text;
@@ -15,7 +18,7 @@ function terminalLink(text: string, url: string, { target = 'stdout', fallback }
     return typeof fallback === 'function' ? fallback(text, url) : `${text} (\u200B${url}\u200B)`;
   }
 
-  return ansiEscapes.link(text, url);
+  return createTerminalLink(text, url);
 }
 
 terminalLink.isSupported = supportsHyperlinks.stdout;
