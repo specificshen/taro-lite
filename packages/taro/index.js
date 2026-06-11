1d97d66f7bc2b074b03fb3908a65fef8c0e3db67
Samuel Ouyang <oys01063538@cainiao.com>
2026-06-11 16:20:08 +0800

    chore: 现代化包入口

--- packages/taro/index.js
+++
@@ -1,9 +0,0 @@
-try {
-  module.exports = require('./dist/index.js');
-} catch (error) {
-  if (error?.code !== 'MODULE_NOT_FOUND' || !/[\\/]dist[\\/]index\.js/.test(error.message)) {
-    throw error;
-  }
-
-  module.exports = require('./src/index.js');
-}
